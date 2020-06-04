a92db8cca40b0a6b4f1737ff834182ff0897fad4
icklej <66431486+icklej@users.noreply.github.com>
2020-06-04 16:55:51 +0100

    Delete app.js

--- safe/js/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("Safe").addEventListener("click",passwordAsk);
-
-function passwordAsk(){
-    alert("This is a safe");
-    var answer = prompt("What is the code");
-    if (answer == "123456"){alert("YES")}else{alert("No")}
-}
-
