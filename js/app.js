93aa90187651fc50cd556b0bd1a165d1d482ee50
icklej <66431486+icklej@users.noreply.github.com>
2020-06-05 21:27:45 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 document.getElementById("Safe").addEventListener("click",passwordAsk);
 
 function passwordAsk(){
-    alert("This is a safe");
+    alert("This is a  big safe");
     var answer = prompt("What is the code");
     if (answer == "ilmtoma"){alert("WINNER - SAY BANOFFEE IN CHAT TO CLAIM")}else{alert("No")}
 }
